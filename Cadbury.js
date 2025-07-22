b207cf0deae13b5897d8e90f88bb7682afcbea45
AkhileshRM <akhilesh.madhyastha09@gmail.com>
2025-07-22 21:46:23 +0530

    Pushed coded

--- Cadbury.js
+++ Cadbury.js
@@ -1,5 +1,5 @@
-let value =  "" || 10  // false, ""  is not accepted by || operator
+let value =  "" || 11  // false, ""  is not accepted by || operator
 console.log(value)
 
-let value1 =  "" ?? 10  // false, "" is accepted by ?? operator Coalescing operator
+let value1 =  "" ?? 11  // false, "" is accepted by ?? operator Coalescing operator
 console.log(value1)

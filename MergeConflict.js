97f74b48286b6f257fd8cb677f26066ce1d048e5
AkhileshRM <akhilesh.madhyastha09@gmail.com>
2025-07-22 19:59:56 +0530

    Student2 done

--- MergeConflict.js
+++ MergeConflict.js
@@ -1,4 +1,4 @@
-//Rest takes in individual values and combines it in an array or object.
+//Rest takes in individual values and combines it in an array or object2.
 
 function Add(a,b,c, ...rest){
     console.log("Rest gives array", rest)
@@ -10,11 +10,11 @@ Add(1,2,3,4,5,6,7)
 
 //Object
 
-var students = {
-    names:"Akhilesh",
+var students2 = {
+    names:"Akhilesh R Madhyastha",
     age:25,
     gender:"male"
 }
 
-const {...rest} = students
+const {...rest} = students2
 console.log(rest)

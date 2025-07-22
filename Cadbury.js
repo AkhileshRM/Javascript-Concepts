a646afbe965b6d0ab46fae6c860d0d33c0a0d3d2
AkhileshRM <akhilesh.madhyastha09@gmail.com>
2025-07-22 21:50:59 +0530

    Teacher Committed

--- Cadbury.js
+++ Cadbury.js
@@ -1,5 +1,5 @@
-let value =  "" || 10  // false, ""  is not accepted by || operator
+let value =  "" || 12  // false, ""  is not accepted by || operator
 console.log(value)
 
-let value1 =  "" ?? 10  // false, "" is accepted by ?? operator Coalescing operator
+let value1 =  "" ?? 12  // false, "" is accepted by ?? operator Coalescing operator
 console.log(value1)
